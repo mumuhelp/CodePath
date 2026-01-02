7860f4d5895f9a76564826f2357feae5e9e5a486
MAZER-shadow <devatyhpasa2@gmail.com>
2026-01-02 12:57:27 +0300

    add first version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
       '/oauth2': {
         target: 'http://localhost:8080',
         changeOrigin: true,
