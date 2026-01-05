9ecb6a21311609f2b0c88a2cd8c5d235b7bb441f
ldpst <lazydolphins@mail.ru>
2026-01-05 13:33:11 +0300

    feat(monolite): add simple test

---
+++ backend/monolite/src/main/java/com/example/Hello.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Hello {
+
+    public String greet(boolean morning) {
+        if (morning) {
+            return "Good morning";
+        } else {
+            return "Hello";
+        }
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
